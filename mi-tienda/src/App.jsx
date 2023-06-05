19c7f480a232d2f1c93585cd9a30a8857da52741
Chuampis <chuampis@gmail.com>
2023-06-05 15:12:36 -0300

    PreEntrega01

--- mi-tienda/src/App.jsx
+++ mi-tienda/src/App.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
 import './App.css'
-import Form from './components/Form/Form'
-
+/* import Form from './components/Form/Form' */
+/* import ItemCount from './components/ItemCount/ItemCount' */
+import NavBar from './components/NavBar/NavBar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 const App = () => {
   let nombre ="Chuampis"
   return (
-    <div>
-      <h1>Hola, soy {nombre}</h1>
-      <h2 style={{color:"red"}}>Bienvenidos a la tienda</h2>
-      <p className='parrafo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure assumenda eveniet sequi vitae hic perspiciatis iusto tempore quisquam ducimus quod repellendus, culpa quidem aliquid minima delectus explicabo quas ut quam? Quae totam atque libero odit officiis tenetur impedit quidem! Fugit!</p>
-      <img src="http://placekitten.com/200/300" alt="" />
-      {
-        /* Este es un comentario */
-      }
-      <Form/> 
-      </div>
+    <>
+    <NavBar/>
+    <ItemListContainer greeting = {"Bienvenidos a la tienda"}/>
+    </>
    )
 }
 
